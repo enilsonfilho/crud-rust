c367e9e22e98d2fb8bfbc2858785ef4ddc2d6646
enilsonfilho <enilsonfilho.dev@hotmail.com>
2023-04-30 17:48:09 -0300

    feat: project

---
+++ README.md
@@ -0,0 +1,24 @@
+### === Sobre o Rust ===
+### Rust é uma linguagem de programação de baixo nível que foi desenvolvida pela Mozilla. 
+### É conhecida por seu sistema de gerenciamento de memória seguro e eficiente, que ajuda os desenvolvedores a escrever códigos livres de falhas de memória
+
+### === Sobre o projeto === 
+### Aplicação de teste desenvolvida em Rust
+### O objetivo da aplicação é conhecer a syntax e estrutura do Rust...
+### O projeto rodará e aprensetará no terminal as manipulações de dados
+
+### === Instalação do Rust ===
+### https://www.rust-lang.org/pt-BR/tools/install
+### Ao executar o exe, escolha a opção 1 para instalação do pacote default
+### Reinicie o cmd/ terminal
+### Para verificar a versão do Rust instalada execute -> cargo --version
+### Para baixar as dependências necessárias do projeto execute -> cargo build
+### Para rodar o projeto execute -> cargo run
+
+### === Detalhes ===
+### O arquivo Cargo.toml é o gerenciador de dependências do projeto - não utilizei nenhuma
+
+### === Observações ===
+### Por ser uma tecnologia que está sendo atualizada constantemente, talvez você tenha alguns problemas com versões...
+### Rust é um tanto quanto complicado para se "brincar", não existem muitos conteúdos da tecnologia na web, qualquer detalhe para de funcionar!
+### Utilizei O VS Code como IDE
---
+++ src/main.rs
@@ -0,0 +1,70 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+struct Moeda {
+    id: u32,
+    nome: String,
+    pais: String,
+}
+
+struct CrudMoeda {
+    moedas: Vec<Moeda>,
+}
+
+impl CrudMoeda {
+    fn novo() -> Self {
+        CrudMoeda {
+            moedas: vec![],
+        }
+    }
+
+    fn adicionar(&mut self, nome: String, pais: String) {
+        let id = self.proximo_id();
+        let moeda = Moeda { id, nome, pais };
+        self.moedas.push(moeda);
+    }
+
+    fn listar(&self) {
+        for moeda in &self.moedas {
+            println!("ID: {}, Nome: {}, País: {}", moeda.id, moeda.nome, moeda.pais);
+        }
+    }
+
+    fn atualizar(&mut self, id: u32, nome: String, pais: String) {
+        for moeda in &mut self.moedas {
+            if moeda.id == id {
+                moeda.nome = nome;
+                moeda.pais = pais;
+                break;
+            }
+        }
+    }
+
+    fn remover(&mut self, id: u32) {
+        self.moedas.retain(|moeda| moeda.id != id);
+    }
+
+    fn proximo_id(&self) -> u32 {
+        self.moedas.last().map_or(0, |moeda| moeda.id) + 1
+    }
+}
+
+fn main() {
+    let mut crud_moeda = CrudMoeda::novo();
+
+    println!("=== Principais Moedas ===");
+
+    crud_moeda.adicionar("Real".to_string(), "Brasil".to_string());
+    crud_moeda.adicionar("Dólar".to_string(), "Estados Unidos".to_string());
+    crud_moeda.adicionar("Euro".to_string(), "União Europeia".to_string());
+
+    println!("Lista de Moedas:");
+    crud_moeda.listar();
+
+    crud_moeda.atualizar(1, "Novo Real".to_string(), "Brasil".to_string());
+    println!("Lista de Moedas após Atualização:");
+    crud_moeda.listar();
+
+    crud_moeda.remover(2);
+    println!("Lista de Moedas após Remoção:");
+    crud_moeda.listar();
+}
